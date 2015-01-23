45a0d3fbf75aa5780b08d356a61f7d858d738abd
narayanprusty <narayanprusty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-23 10:00:18 +0000

    fixed some issues
    git-svn-id: https://plugins.svn.wordpress.org/googles-no-captcha-recaptcha/trunk@1073968 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- google-no-captcha-recaptcha.php
+++ google-no-captcha-recaptcha.php
@@ -19,18 +19,9 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-    function add_new_menu_items()
+        function no_captcha_recaptcha_menu()
     {
-        add_menu_page(
-            "reCapatcha Options",
-            "reCaptcha Options",
-            "manage_options",
-            "recaptcha-options",
-            "recaptcha_options_page",
-            "",
-            100
-        );
-
+        add_menu_page( "reCapatcha Options", "reCaptcha Options", "manage_options", "recaptcha-options", "recaptcha_options_page", "", 100 );
     }
 
     function recaptcha_options_page()
@@ -40,8 +31,8 @@
             <h1>reCaptcha Options</h1>
             <form method="post" action="options.php">
                 <?php
-                    settings_fields("header_section");
-                    do_settings_sections("recaptcha-options");
+                    settings_fields( "header_section" );
+                    do_settings_sections( "recaptcha-options" );
                     submit_button();                     
                 ?>          
             </form>
@@ -49,23 +40,23 @@
         <?php
     }
 
-    add_action("admin_menu", "add_new_menu_items");
+    add_action( "admin_menu", "no_captcha_recaptcha_menu" );
 
-    function display_options()
+    function display_recaptcha_options()
     {
-        add_settings_section("header_section", "Keys", "display_options_content", "recaptcha-options");
+        add_settings_section( "header_section", "Keys", "display_recaptcha_content", "recaptcha-options" );
 
-        add_settings_field("captcha_site_key", __("Site Key"), "display_captcha_site_key_element", "recaptcha-options", "header_section");
-        add_settings_field("captcha_secret_key", __("Secret Key"), "display_captcha_secret_key_element", "recaptcha-options", "header_section");
+        add_settings_field( "captcha_site_key", __("Site Key"), "display_captcha_site_key_element", "recaptcha-options", "header_section" );
+        add_settings_field( "captcha_secret_key", __("Secret Key"), "display_captcha_secret_key_element", "recaptcha-options", "header_section" );
 
-        register_setting("header_section", "captcha_site_key");
-        register_setting("header_section", "captcha_secret_key");
+        register_setting( "header_section", "captcha_site_key" );
+        register_setting( "header_section", "captcha_secret_key" );
     }
 
-    function display_options_content()
+    function display_recaptcha_content()
     {
-        echo __('<p>You need to <a href="https://www.google.com/recaptcha/admin" rel="external">register you domain</a> and get keys to make this plugin work.</p>');
-        echo __("Enter the key details below");
+        echo __( '<p>You need to <a href="https://www.google.com/recaptcha/admin" rel="external">register you domain</a> and get keys to make this plugin work.</p>' );
+        echo __( "Enter the key details below" );
     }
     function display_captcha_site_key_element()
     {
@@ -80,196 +71,203 @@
         <?php
     }
 
-    add_action("admin_init", "display_options");
+    add_action( "admin_init", "display_recaptcha_options" );
 
-add_action("wp_enqueue_scripts", "frontend_recaptcha_script");
+    add_action( "wp_enqueue_scripts", "frontend_recaptcha_script" );
 
-function frontend_recaptcha_script()
-{
-    wp_register_script("recaptcha", "https://www.google.com/recaptcha/api.js");
-    wp_enqueue_script("recaptcha");
-}
-
-add_action("comment_form", "display_comment_recaptcha");
-
-function display_comment_recaptcha()
-{
-    ?>
-        <style>
-            #commentform #submit
-            {
-                display: none;
-            }
-        </style>
-        <div class="g-recaptcha" data-sitekey="<?php echo get_option('captcha_site_key'); ?>"></div>
-        <input name="submit" type="submit" value="Submit Comment">
-    <?php
-}
-
-add_filter("preprocess_comment", "verify_comment_captcha");
-
-function verify_comment_captcha($commentdata)
-{
-    if(defined('XMLRPC_REQUEST')) 
-    { 
-        // don't verify captcha at XMLRPC_REQUESTS
-        return $commentdata;
-    } 
-    else 
+    function frontend_recaptcha_script()
     {
-        if(isset($_POST['g-recaptcha-response']))
+        if( get_option( 'captcha_site_key' ) && get_option( 'captcha_secret_key' ) )
         {
-            $recaptcha_secret = get_option('captcha_secret_key');
-            $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$recaptcha_secret."&response=".$_POST['g-recaptcha-response']);
-            $response = json_decode($response, true);
-            if($response["success"] == true)
+            wp_register_script( "recaptcha", "https://www.google.com/recaptcha/api.js" );
+            wp_enqueue_script( "recaptcha" );
+
+            $plugin_url = plugin_dir_url( __FILE__ );
+            
+            wp_enqueue_style( "no-captcha-recaptcha", $plugin_url . "style.css" );
+        }   
+    }
+
+    add_action( "comment_form", "display_comment_recaptcha" );
+
+    function display_comment_recaptcha()
+    {
+        if( get_option( 'captcha_site_key' ) && get_option( 'captcha_secret_key' ) )
+        {
+            ?>
+                <div class="g-recaptcha" data-sitekey="<?php echo get_option( 'captcha_site_key' ); ?>"></div>
+                <input name="submit" type="submit" value="Submit Comment">
+            <?php   
+        }
+        
+    }
+
+    add_filter( "preprocess_comment", "verify_comment_captcha" );
+
+    function verify_comment_captcha( $commentdata )
+    {
+        if( isset( $_POST['g-recaptcha-response'] ) )
+        {
+            $recaptcha_secret = get_option( 'captcha_secret_key' );
+            $response = file_get_contents( "https://www.google.com/recaptcha/api/siteverify?secret=" . $recaptcha_secret . "&response=" .$_POST['g-recaptcha-response'] );
+            $response = json_decode( $response, true );
+            if( true == $response["success"] )
             {
                 return $commentdata;
             }
             else
             {
-                echo __("Bots are not allowed to submit comments.");
+                echo __( "Bots are not allowed to submit comments." );
                 return null;
             }
         }
         else
         {
-            echo __("Bots are not allowed to submit comments. If you are not a bot then please enable JavaScript in browser.");
-            return null;
+            if( get_option( 'captcha_site_key' ) && get_option( 'captcha_secret_key' ) )
+            {
+                echo __( "Bots are not allowed to submit comments. If you are not a bot then please enable JavaScript in browser." );
+                return null;    
+            }   
+            else
+            {
+                return $commentdata;
+            }
         }
     }
-}
 
+    add_action( "login_enqueue_scripts", "login_recaptcha_script" );
 
-add_action("login_enqueue_scripts", "login_recaptcha_script");
-
-function login_recaptcha_script()
-{
-    wp_register_script("recaptcha_login", "https://www.google.com/recaptcha/api.js");
-    wp_enqueue_script("recaptcha_login");
-}
-
-add_action("login_form", "display_login_captcha");
-
-function display_login_captcha()
-{
-
-    ?>
-        <div class="g-recaptcha" data-sitekey="<?php echo get_option('captcha_site_key'); ?>"></div>
-        <br>
-    <?php
-}
-
-add_filter("wp_authenticate_user", "verify_login_captcha", 10, 2);
-
-function verify_login_captcha($user, $password)
-{
-    if(defined('XMLRPC_REQUEST')) 
+    function login_recaptcha_script()
     {
-        return $user;
-    } 
-    else 
-    {
-        if(isset($_POST['g-recaptcha-response']))
+        if( get_option( 'captcha_site_key' ) && get_option( 'captcha_secret_key' ) )
         {
-            $recaptcha_secret = get_option('captcha_secret_key');
-            $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$recaptcha_secret."&response=".$_POST['g-recaptcha-response']);
-            $response = json_decode($response, true);
-            if($response["success"] == true)
+            wp_register_script( "recaptcha_login", "https://www.google.com/recaptcha/api.js" );
+            wp_enqueue_script( "recaptcha_login" );
+        }
+    }
+
+    add_action( "login_form", "display_login_captcha" );
+
+    function display_login_captcha()
+    {
+        if( get_option( 'captcha_site_key' ) && get_option( 'captcha_secret_key' ) )
+        {
+            ?>
+                <div class="g-recaptcha" data-sitekey="<?php echo get_option('captcha_site_key' ); ?>"></div>
+            <?php
+        }   
+    }
+
+    add_filter( "wp_authenticate_user", "verify_login_captcha", 10, 2 );
+
+    function verify_login_captcha( $user, $password )
+    {
+        if( isset( $_POST['g-recaptcha-response'] ) )
+        {
+            $recaptcha_secret = get_option( 'captcha_secret_key' );
+            $response = file_get_contents( "https://www.google.com/recaptcha/api/siteverify?secret=" . $recaptcha_secret . "&response=" . $_POST['g-recaptcha-response'] );
+            $response = json_decode( $response, true );
+            if( true == $response["success"] )
             {
                 return $user;
             }
             else
             {
-                return new WP_Error("Captcha Invalid", __("<strong>ERROR</strong>: You are a bot"));
-            }
+                return new WP_Error( "Captcha Invalid", __( "<strong>ERROR</strong>: You are a bot" ) );
+            } 
         }
         else
         {
-            return new WP_Error("Captcha Invalid", __("<strong>ERROR</strong>: You are a bot. If not then enable JavaScript"));
-        }
-    }   
-}
+            if( get_option( 'captcha_site_key' ) && get_option( 'captcha_secret_key' ) )
+            {
+                return new WP_Error( "Captcha Invalid", __( "<strong>ERROR</strong>: You are a bot. If not then enable JavaScript" ) );
+            }
+            else
+            {
+                return $user;
+            }
+        }   
+    }
 
-add_action("register_form", "display_register_captcha");
+    add_action( "register_form", "display_register_captcha" );
 
-function display_register_captcha()
-{
-     
-    ?>
-        
-        <div class="g-recaptcha" data-sitekey="<?php echo get_option('captcha_site_key'); ?>"></div>
-        <br>
-    <?php
-}
 
-add_filter("registration_errors", "verify_registration_captcha", 10, 3);
-
-function verify_registration_captcha($errors, $sanitized_user_login, $user_email)
-{
-    if ( defined('XMLRPC_REQUEST') ) 
-    { 
-        // don't verify captcha at XMLRPC_REQUESTS
-        return $errors;
-    } 
-    else 
+    function display_register_captcha()
     {
-            // do your code here
-        if(isset($_POST['g-recaptcha-response']))
+        if( get_option( 'captcha_site_key' ) && get_option( 'captcha_secret_key' ) )
         {
-            $recaptcha_secret = get_option('captcha_secret_key');
-            $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$recaptcha_secret."&response=".$_POST['g-recaptcha-response']);
-            $response = json_decode($response, true);
-            if($response["success"] == true)
+            ?>
+                <div class="g-recaptcha" data-sitekey="<?php echo get_option( 'captcha_site_key' ); ?>"></div>
+            <?php   
+        }       
+    }
+
+    add_filter( "registration_errors", "verify_registration_captcha", 10, 3 );
+
+    function verify_registration_captcha( $errors, $sanitized_user_login, $user_email )
+    {
+        if( isset( $_POST['g-recaptcha-response'] ) )
+        {
+            $recaptcha_secret = get_option( 'captcha_secret_key' );
+            $response = file_get_contents( "https://www.google.com/recaptcha/api/siteverify?secret=" . $recaptcha_secret . "&response=" . $_POST['g-recaptcha-response'] );
+            $response = json_decode( $response, true );
+            if( true == $response["success"] )
             {
                 return $errors;
             }
             else
             {
-                $errors->add("Captcha Invalid", __("<strong>ERROR</strong>: You are a bot"));
+                $errors->add( "Captcha Invalid", __( "<strong>ERROR</strong>: You are a bot" ) );
             }
         }
         else
-        {
-            $errors->add("Captcha Invalid", __("<strong>ERROR</strong>: You are a bot. If not then enable JavaScript"));
-        }
+        {   
+            if( get_option( 'captcha_site_key' ) && get_option( 'captcha_secret_key' ) )
+            {
+                $errors->add( "Captcha Invalid", __( "<strong>ERROR</strong>: You are a bot. If not then enable JavaScript" ) );
+            }
+            else
+            {
+                return $errors;
+            }
+            
+        }   
+
+        return $errors;
     }
-        
 
-    return $errors;
-}
 
-add_action("lostpassword_form", "display_login_captcha");
-add_action("lostpassword_post", "verify_lostpassword_captcha");
+    add_action( "lostpassword_form", "display_login_captcha" );
+    add_action( "lostpassword_post", "verify_lostpassword_captcha" );
 
-function verify_lostpassword_captcha()
-{
-    if ( defined('XMLRPC_REQUEST') ) 
+    function verify_lostpassword_captcha()
     {
-        return;
-    } 
-    else 
-    {
-            // do your code here
-        if(isset($_POST['g-recaptcha-response']))
+        if( isset( $_POST['g-recaptcha-response'] ) )
         {
-            $recaptcha_secret = get_option('captcha_secret_key');
-            $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$recaptcha_secret."&response=".$_POST['g-recaptcha-response']);
-            $response = json_decode($response, true);
-            if($response["success"] == true)
+            $recaptcha_secret = get_option( 'captcha_secret_key' );
+            $response = file_get_contents( "https://www.google.com/recaptcha/api/siteverify?secret=" . $recaptcha_secret . "&response=" . $_POST['g-recaptcha-response'] );
+            $response = json_decode( $response, true );
+            if( true == $response["success"] )
             {
                 return;
             }
             else
             {
-                wp_die(__("<strong>ERROR</strong>: You are a bot"));
+                wp_die( __( "<strong>ERROR</strong>: You are a bot" ) );
             }
         }
         else
         {
-            wp_die(__("<strong>ERROR</strong>: You are a bot. If not then enable JavaScript"));
-        }
-    }   
+            if( get_option( 'captcha_site_key' ) && get_option( 'captcha_secret_key' ) )
+            {
+                wp_die( __( "<strong>ERROR</strong>: You are a bot. If not then enable JavaScript" ) ); 
+            }
+            else
+            {
+                return;
+            }
+            
+        }   
 
-    return; 
-}
+        return $errors; 
+    }
